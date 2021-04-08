96baf59a8903fdf2d7a1a6f1fb61eb46e377a477
Rabiulislam75 <robiulislammamon42@gmail.com>
2021-04-08 23:10:47 +0600

    Initial Commit

---
+++ src/components/child/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './style.css'
+
+class Child extends Component{
+    render(){
+        return(
+            <div className="Conteiner">
+                 <h1>Hello World!</h1>
+            </div>
+            
+        )
+    }
+
+}
+export default Child
